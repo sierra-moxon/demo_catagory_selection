fc8a7a9b71a2f4f07094954f7848aa93a2c34147
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-08-18 11:58:24 -0700

    pseudo testing code

--- src/demo_category_selection/main.py
+++ src/demo_category_selection/main.py
@@ -6,19 +6,23 @@ from oaklib import get_adapter
 
 def demo():
     """
-    navigate phenio and biolink together
+    navigate pato and biolink together
 
     """
 
     tk = Toolkit()
-    adapter = get_adapter("sqlite:obo:pato")
-
-    for entity in adapter.entities():
+    adapter = get_adapter("sqlite:obo:upheno")
+    test_entities = ["PATO:0000001", "PATO:0000002", "UPHENO:0075852", "UPHENO:0001002"]
+    for entity in test_entities:
         entity_parents = adapter.hierarchical_parents(entity, isa_only=True)
+        for aroot in adapter.roots():
+            print(entity, aroot)
+        print("parents of", entity, entity_parents)
         if entity_parents:
             for parent in entity_parents:
                 bl_category = get_biolink_category(parent, tk)
                 if bl_category:
+                    return bl_category
 
 
 def get_biolink_category(entity, tk):
@@ -27,9 +31,10 @@ def get_biolink_category(entity, tk):
 
     """
 
+    bl_element = tk.get_element_by_mapping(identifier=str(entity))
     if tk.get_element_by_mapping(identifier=str(entity)) is not None:
         print("found category for entity", entity, tk.get_element_by_mapping(identifier=str(entity)))
-        return tk.get_element_by_mapping(identifier=str(entity))
+        return bl_element
 
 
 if __name__ == "__main__":
